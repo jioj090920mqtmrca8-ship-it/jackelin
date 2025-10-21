59d6d90d3b37c35e4dd73c42cec1330b73a2529a
jioj090920mqtmrca8-ship-it <jioj090920mqtmrca8@lake.edu.mx>
2025-10-21 13:05:44 -0600

    1.- suma de tres  numeros

---
+++ 2.- suma de tres numeros.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main(){
+ int numero1;
+ int numero2;
+ int numero3;
+ int suma;
+
+printf("ingresa el valor de nuemro1: \n");
+scanf("%d",&numero1);
+
+printf("ingresa el valor de numero2:\n");
+scanf("%d",&numero2);
+
+printf("ingresa el valor de un numero3");
+scanf("%d",&numero3);
+
+suma= numero1 + numero2+ numero3;
+
+printf("la suma de ,los 3 numeros es:%d",suma);
+}
